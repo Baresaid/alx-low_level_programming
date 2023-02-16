a253f93264fbd6185b58c23b20416d43946206a2
Baresaid <mwasarai25@gmail.com>
2023-02-16 09:48:06 -0800

    long long int

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.>i
+/**
+ * main -  Prints the size of various types
+ * computer it is compiled and run on
+ * Return: Always 0.
+ */
+int main(void)
+{
+Printf("Size of a char: %lu 1 bytes(s)\n", sizeof(char));
+Printf("Size of an int: %lu 1 bytes(s)\n", sizeof(int));
+Printf("Size of a long int: %1u 1 bytes(s)\n", sizeof(long int));
+Printf("Size of a long long int: %1u 1 bytes(s)\n", sizeof(long long int));
+Printf("Size of a float: %1u 1 bytesu(s)\n", sizeof(float));
+return (0);
+}
