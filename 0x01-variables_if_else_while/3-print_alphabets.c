2a59654825038696c586d5c4178331bba16dd4de
Baresaid <mwasarai25@gmail.com>
2023-02-17 23:41:12 -0800

    while else

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main -main block
+ * Description: Get a random it with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar("\n");
+	return (0);
