446ae831c7c6acfa9d5ce3e560e9e14ded61bd81
Baresaid <mwasarai25@gmail.com>
2023-02-18 09:43:47 -0800

    mwasarai said

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main -main block
+ * Description: Get a random it with 5
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int c;
+
+	c = 0;
+	while
+		  (c < 10) {
+			  putchar(c + '0');
+			  c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
