e3cf35d91dd3052591fb58c1126ad2eda503c4bc
Baresaid <mwasarai25@gmail.com>
2023-02-17 23:54:25 -0800

    bare

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,15 @@
  */
 
 int main(void)
+
 {
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
+	putchar(low);
 
 	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
-	putchar("\n");
+	putchar('\n');
 	return (0);
+}
