ac514e8a6c6b479a935a105f26fee0c6fb2ca4b5
Baresaid <mwasarai25@gmail.com>
2023-02-21 21:56:10 -0800

    mwasarai

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks for the alphabet characters
+ * @c: the character to be checked
+ * Return: 1
+ */
+int _islower(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
