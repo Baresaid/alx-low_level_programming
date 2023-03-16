41fa65c25918867eb5566c5719cadf41fb1de35f
Baresaid <mwasarai25@gmail.com>
2023-03-16 01:45:41 -0700

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
+/**
+ * Return: always return (0)
+ */
 
-int main(void) {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 37);
-    return 1;
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 37);
+	return 1;
 }
