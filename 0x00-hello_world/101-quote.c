e7d02f1f85610e518f19bc0a9662f6a1585b3858
Baresaid <mwasarai25@gmail.com>
2023-03-16 01:49:37 -0700

    different changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 /**
+ * main - program that prints exactly 
  * Return: always return (0)
  */
 
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, 37);
-	return 1;
+
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, 37);
+return 1;
 }
